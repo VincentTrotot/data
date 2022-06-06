fed84c466a609abb8cb03641d5c3aa76c4608fd0
Vincent TROTOT <vincent.trotot@icloud.com>
2022-06-06 14:50:53 +0200

    👌🏼 IMPROVE: utilisation chartjs via symfony ux

--- src/Controller/App/AppController.php
+++ src/Controller/App/AppController.php
@@ -2,30 +2,44 @@
 
 namespace App\Controller\App;
 
+use Symfony\UX\Chartjs\Model\Chart;
 use App\Repository\Carburant\PleinRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(PleinRepository $pleinRepository): Response
+    public function index(PleinRepository $pleinRepository, ChartBuilderInterface $chartBuilder): Response
     {
         $pleins = $pleinRepository->findAllByDate('ASC');
         $labels = [];
         $data = [];
+
+
         foreach ($pleins as $plein) {
             $labels[] = $plein->getDate()->format('m/Y');
             $data[] = $plein->getPrixAuLitre();
         }
+
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $chart->setData([
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => 'Prix du litre',
+                    'backgroundColor' => 'rgb(0, 0, 75)',
+                    'borderColor' => 'rgb(0, 0, 75)',
+                    'data' => $data,
+                ],
+            ],
+        ]);
+
         return $this->render('app/index.html.twig', [
-            'pleins' => $pleins,
-            'data' => json_encode($data),
-            'labels' => json_encode($labels),
+            'chart' => $chart,
         ]);
     }
-
-    
-
 }
